1fc00365dd4d623541048e59a7f2974000932697
Vedant2113 <vedantpatelparesh@gmail.com>
2025-04-16 15:44:28 -0400

    Update data_loader.py

--- app/data_loader.py
+++ app/data_loader.py
@@ -1,1 +1,7 @@
+# app/data_loader.py
+import pandas as pd
 
+def load_data(file_path):
+    df = pd.read_excel(file_path)
+    df['Time'] = pd.to_datetime(df['DepartTime'], errors='coerce').dt.time
+    return df
