c36f63697e32cbdb2928e47ee854f189858b7458
Vedant2113 <vedantpatelparesh@gmail.com>
2025-04-16 15:43:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+import streamlit as st
+import pandas as pd
+from app.data_loader import load_data
+from app.graph_builder import build_graph
+from app.routes import find_shortest_route
 
+st.set_page_config(page_title="Bus Route Optimizer", layout="wide")
+st.title("🚌 Bus Route Optimizer")
